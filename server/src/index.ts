3356df7629c656d560ac2c78d714bbff4b7a2b3d
digmi123 <65775879+digmi123@users.noreply.github.com>
2026-02-19 08:49:40 +0200

    Create the entry point of the server.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,22 @@
-import express, { Request, Response } from 'express';
+import express from "express";
+import tenantsRouter from "./tenants/routes";
+import offersRouter from "./offers/routes";
+import { authMiddleware } from "./middlewares/authorization";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
-});
+// Middleware;
+app.use(express.json());
+app.use(authMiddleware);
 
-app.listen(port, () => {
+// Routes;
+app.use("/tenants", tenantsRouter);
+app.use("/offers", offersRouter);
+
+const server = app.listen(port, (error) => {
+  if (error) {
+    console.error("Failed to start server:", error);
+  }
   console.log(`Server running at http://localhost:${port}`);
 });
