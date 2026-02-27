e8363907204d64c295bdc5d37d4a465cd0fc7a0b
digmi123 <65775879+digmi123@users.noreply.github.com>
2026-02-27 19:08:58 +0200

    Update purchase route validation to handle purchase of specific variant.

--- server/src/purchase/validation.ts
+++ server/src/purchase/validation.ts
@@ -2,22 +2,27 @@ import { Request } from "express";
 import { z } from "zod";
 import { AppError } from "../errors/AppError";
 
-const bodyParamsSchema = z.object({
-  tenantId: z.string().optional(),
-  offerId: z.uuid("Invalid offer ID format"),
-  email: z.email("Invalid email format"),
-  amount: z.coerce.number().positive("The provided amount is invalid"),
-  buyer_name: z.string().min(1, "Buyer name is required"),
-  buyer_email: z.email("Invalid buyer email format"),
-  buyer_phone: z.string().optional(),
-  receiptDetails: z.object({
-    fullName: z.string().min(1, "Full Name is required"),
+const bodyParamsSchema = z
+  .object({
+    tenantId: z.string().optional(),
+    offerId: z.uuid("Invalid offer ID format").optional(),
+    offerVariantId: z.uuid("Invalid offer variant ID format").optional(),
     email: z.email("Invalid email format"),
-    phone: z.string().optional(),
-    taxId: z.string().optional(),
-    noted: z.string().optional(),
-  }),
-});
+    amount: z.coerce.number().positive("The provided amount is invalid"),
+    buyer_name: z.string().min(1, "Buyer name is required"),
+    buyer_email: z.email("Invalid buyer email format"),
+    buyer_phone: z.string().optional(),
+    receiptDetails: z.object({
+      fullName: z.string().min(1, "Full Name is required"),
+      email: z.email("Invalid email format"),
+      phone: z.string().optional(),
+      taxId: z.string().optional(),
+      noted: z.string().optional(),
+    }),
+  })
+  .refine((data) => data.offerId || data.offerVariantId, {
+    message: "Either offerId or offerVariantId must be provided",
+  });
 
 export type PurchaseRequestData = z.infer<typeof bodyParamsSchema>;
 
