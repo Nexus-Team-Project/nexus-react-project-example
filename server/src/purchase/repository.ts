c3550f686e899d99f0868a1fba6acb98c595d0c3
digmi123 <65775879+digmi123@users.noreply.github.com>
2026-02-27 19:15:02 +0200

    On purchase save the variant and the expiration_date.

--- server/src/purchase/repository.ts
+++ server/src/purchase/repository.ts
@@ -11,10 +11,16 @@ export const savePurchase = async (
   data: PurchaseRequestData,
   payme: PaymeSaleResponse,
   userId: string,
+  context: {
+    offerId: string;
+    offerVariantId?: string;
+    expiration_date: Date | null;
+  },
 ) => {
   return await prisma.purchase.create({
     data: {
-      offer_id: data.offerId,
+      offer_id: context.offerId,
+      offer_variant_id: context.offerVariantId,
       user_id: userId,
       tenant_id: data.tenantId,
       amount: data.amount,
@@ -23,6 +29,7 @@ export const savePurchase = async (
       payme_sale_code: payme.payme_sale_code,
       sale_url: payme.sale_url,
       receipt_details: data.receiptDetails,
+      expiration_date: context.expiration_date,
     },
   });
 };
@@ -36,24 +43,6 @@ export const updatePurchaseByCallback = async (
     data: {
       status: data.status,
       payme_transaction_id: data.payme_transaction_id,
-    },
-  });
-};
-
-export const getOffer = async (offerId: string) => {
-  return await prisma.merchantsOffers.findUnique({
-    where: { id: offerId },
-    select: {
-      status: true,
-      available_quantity: true,
-      title: true,
-      merchant: {
-        select: {
-          payme_seller_id: true,
-          payme_api_key: true,
-          commission_rate: true,
-        },
-      },
     },
   });
 };
